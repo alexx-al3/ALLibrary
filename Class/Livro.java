769077572a1296ceda951697ce3ee1e1f4a49b0b
Alex Alves <alexsandro.allexx@gmail.com>
2026-04-19 21:43:22 -0300

    .
     .

--- Class/Livro.java
+++ Class/Livro.java
@@ -1,6 +1,32 @@
 package Class;
 
 
+class Livro {
+
+    private String nome, autor;
+    private int id;
+
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public String getAutor() {
+        return autor;
+    }
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    
+}
+
 
 
 
