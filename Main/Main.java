401671973ff9730809f841b5d0d52db039d1efed
Alex Alves <alexsandro.allexx@gmail.com>
2026-04-21 15:47:04 -0300

    conection

---
+++ Main/Main.java
@@ -0,0 +1,20 @@
+package Main;
+import com.allibrary.connection.ConnectionFactory;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class Main {
+    public static void main(String[] args) {
+
+        try (Connection conn = ConnectionFactory.getConnection()) {
+
+            if (conn != null && !conn.isClosed()) {
+                System.out.println("✅ Conectado com sucesso ao MySQL!");
+            }
+
+        } catch (SQLException e) {
+            System.out.println("❌ Erro ao conectar:");
+            e.printStackTrace();
+        }
+    }
+}
---
+++ com/allibrary/connection/ConnectionFactory.java
@@ -0,0 +1,20 @@
+package com.allibrary.connection;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionFactory {
+
+    private static final String URL = "jdbc:mysql://localhost:3306/allibrary?useSSL=false&serverTimezone=UTC";
+    private static final String USER = "root";
+    private static final String PASSWORD = "root";
+
+    public static Connection getConnection() {
+        try {
+            return DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch (SQLException e) {
+            throw new RuntimeException("Erro na conexão com o banco de dados", e);
+        }
+    }
+}
